8fd6c3fb86e96bd1451a2bc2777af48b1ad9e134
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2022-03-14 13:46:26 +0100

    commit en master

--- src/com/gitRamas/Main.java
+++ src/com/gitRamas/Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println("2");
         System.out.println("3");
         //creado repositorio
+        System.out.println("commit");
+        System.out.println("en");
+        System.out.println("master 2");
     }
 }
